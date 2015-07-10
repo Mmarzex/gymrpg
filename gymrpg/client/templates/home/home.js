6378ec986b2b38671daf62c1a6089463e029a811
Max <sync95@gmail.com>
2015-07-10 15:09:25 -0400

    Fitbit stuff working sort of

--- gymrpg/client/templates/home/home.js
+++ gymrpg/client/templates/home/home.js
@@ -11,8 +11,8 @@ Template.home.rendered = function() {
 
 	ServiceConfiguration.configurations.insert({
 		service: "fitbit",
-		clientKey: "e9d83c7bfc19e1edef8619dc6f1cc8b0",
-		clientSecret: "56fe0cb799b7e38b90a8a4fd1801d6e8"
+		consumerKey: "e9d83c7bfc19e1edef8619dc6f1cc8b0",
+		secret: "56fe0cb799b7e38b90a8a4fd1801d6e8"
 	});
 };
 
@@ -21,11 +21,34 @@ Template.home.events({
 	'click #fitbitprofile': function(e){
 		e.preventDefault();
 		console.log("Inside click event");
-		console.log(Meteor.user().services.fitbit.accessToken);
-		console.log(Meteor.http.call("GET", "https://api.fitbit.com/1/user/-/profile.json", 
-			{timeout: 3000,
-			}, function(err, result){
-			console.log(result);
-		}));
+		var steps = Meteor.call('mySteps');
+		console.log (steps);
+		// console.log(Meteor.user().services.fitbit.accessTokenSecret);
+		// console.log(new Fitbit());
+		// var authkey = "OAuth oauth_consumer_key=\"e9d83c7bfc19e1edef8619dc6f1cc8b0\","
+  //         + "oauth_nonce=\"kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg\","
+  //         + "oauth_signature=\"tnnArxj06cWHq44gCs1OSKk%2FjLY%3D\"," 
+  //         + "oauth_signature_method=\"HMAC-SHA1\"," 
+  //         + "oauth_timestamp=\"" +(new Date().getTime()/1000).toFixed(0)+ "\"," 
+  //         + "oauth_token=\"" + Meteor.user().services.fitbit.accessToken + "\"," 
+  //         + "oauth_version=\"1.0\"";
+  // 		var config = Accounts.loginServiceConfiguration.findOne({service: 'fitbit'});
+  // 		config.secret = config.consumerSecret = '56fe0cb799b7e38b90a8a4fd1801d6e8';
+  // 		var url = 'https://api.fitbit.com/1/user/-/profile.json';
+  // 		var authtest = new OAuth1Binding(config, url);
+  		
+  // 		authtest.accessToken = Meteor.user().services.fitbit.accessToken;
+  // 		authtest.accessTokenSecret = Meteor.user().services.fitbit.accessTokenSecret;
+  // 		console.log(config.consumerSecret);
+		// console.log(authtest);
+		// console.log(authtest.get(url).data)
+		// console.log(Meteor.http.call("GET", "https://api.fitbit.com/1/user/-/profile.json", 
+		// 	{timeout: 3000,
+		// 		headers: {
+		// 			Authorization: authkey
+		// 		}
+		// 	}, function(err, result){
+		// 	console.log(result);
+		// }));
 	}
 });
--- gymrpg/server/config.js
+++ gymrpg/server/config.js
@@ -1,3 +1,38 @@
+
+var fitbit = new Fitbit();
+
+Meteor.startup(function() {
+	ServiceConfiguration.configurations.remove({
+		service: "fitbit"
+	});
+
+	ServiceConfiguration.configurations.insert({
+		service: "fitbit",
+		consumerKey: "e9d83c7bfc19e1edef8619dc6f1cc8b0",
+		secret: "56fe0cb799b7e38b90a8a4fd1801d6e8"
+	});
+});
+
+Meteor.methods({
+	mySteps: function() {
+
+		var data = fitbit.getSteps().data;
+		console.log("Inside mySteps");
+		console.log(fitbit.getSteps());
+		// for (var key in data["activities-steps"])
+		// 	ActivitiesSteps.update(
+		// 		{ dateTime: data["activities-steps"][key]["dateTime"]},
+		// 			{
+		// 				$set: {
+		// 					dateTime: data["activities-steps"][key]["dateTime"],
+		// 					value: data["activities-steps"][key]["value"]
+		// 				}					
+		// 			},
+		// 		{ upsert: true }
+		// 	)
+	}
+});
+
 Meteor.publish("currentAccessToken", function(){
-	return Meteor.users.find(this.userId, {fields: {'services.fitbit.accessToken': 1}});
+	return Meteor.users.find(this.userId, {fields: {'services.fitbit.accessToken': 1, 'services.fitbit.accessTokenSecret': 1}});
 });;
